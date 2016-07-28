276a89a8b07d82076d0447162ab9e36f3ae0255c
Zachary J Rollyson <zach@izimobile.com>
2016-07-27 21:45:23 -0400

    Correct startup log typo.

--- server.js
+++ server.js
@@ -3,4 +3,4 @@
 const app = require('./src/app');
 const config = require('./src/config');
 
-app.listen(config.apiPort, () => sjLog(`Listening at ${config.api}`));
+app.listen(config.apiPort, () => sjLog(`Listening at ${config.apiBase}:${config.apiPort}`));
--- src/config/base.js
+++ src/config/base.js
@@ -4,6 +4,5 @@ module.exports = {
   appName: 'Sojourner',
   apiPort: 1996,
   apiBase: 'http://localhost',
-  apiUrl: `${this.apiBase}:${this.apiPort}`
 };
 
