7e67df80995f7e7d5e640837313c86d4ebd515b7
Zachary J Rollyson <zach@izimobile.com>
2016-07-27 22:58:50 -0400

    Add functions & tests for creating new rover.

--- src/lib/rover/rover.controller.js
+++ src/lib/rover/rover.controller.js
@@ -1,9 +1,19 @@
 'use strict';
 
+const roverProto = {
+  x: 0,
+  y: 0,
+  direction: 'N'
+};
+
+// TODO… storing these in memory isn't really sustainable (but it is simple).
+const rovers = [];
+
 module.exports = {
   '/': {
     get(req, res) {
-      res.sjPass('Hello from rover!');
+      const rover = Object.assign({}, roverProto, { id: rovers.length });
+      res.sjPass(rover);
     }
   }
 };
--- test/rover.js
+++ test/rover.js
@@ -6,21 +6,32 @@ const stub = require('../src/lib/resHandler').stub;
 const controller = require('../src/lib/rover/rover.controller');
 
 suite('lib/rover', () => {
-  suite('GET /user', () => {
-    suite('Check in', () => {
+  suite('GET /rover', () => {
+    suite('Get starting location and direction of new rover.', () => {
       let actual;
 
       suiteSetup('', done => {
         controller['/'].get(null, stub(res => {
-          actual = res;
+          actual = res.data;
           done();
         }));
       });
 
-      test('should get simple string data', () => {
-        assert.equal(actual.data, 'Hello from rover!');
+      test('x position should equal 0', () => {
+        assert.equal(actual.x, 0);
       });
 
+      test('y position should equal 0', () => {
+        assert.equal(actual.y, 0);
+      });
+
+      test('direction should equal N', () => {
+        assert.equal(actual.direction, 'N');
+      });
+
+      test('id should equal 0', () => {
+        assert.equal(actual.id, 0);
+      });
     });
   });
 });
