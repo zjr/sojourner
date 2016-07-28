a74207b5487eb15ad1be732fa1e204c822ba662f
Zachary J Rollyson <zach@izimobile.com>
2016-07-27 21:25:09 -0400

    Add currently fairly unnecessary config setup.

---
+++ src/config/base.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = {
+  appName: 'Sojourner',
+  apiUrl: 'http://localhost:1996'
+};
+
---
+++ src/config/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const fs = require('fs');
+const baseCfg = require('./base');
+
+const envs = fs.readdirSync('./envs').map(env => env.replace('.js', ''));
+
+function build(APP_ENV = process.env.APP_ENV || 'dev') {
+  if (!envs.includes(APP_ENV)) {
+    return new Error(`APP_ENV "${APP_ENV}" is not allowed.`);
+  }
+
+  const envCfg = require(`./envs/${APP_ENV}`);
+
+  return Object.freeze(Object.assign({}, baseCfg, envCfg));
+}
+
+module.exports = { build, default: build() };
